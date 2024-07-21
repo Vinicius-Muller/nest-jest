92a14a52faa74a67057d2299a906552bd52db64f
vinicius <viniciusmul98@gmail.com>
2024-07-21 02:20:29 -0300

    migration v1

--- src/config/migrations/1721538230801-updateUsers.ts
+++ src/config/migrations/1721538230801-updateUsers.ts
@@ -4,7 +4,7 @@ export class UpdateUsers1721538230801 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         ALTER TABLE users 
-        ALTER COLUMN permissions SET DATA TYPE UUID[] USING permissions::UUID[];
+        ALTER COLUMN permissions SET DATA TYPE UUID[];
       `);
   }
 
